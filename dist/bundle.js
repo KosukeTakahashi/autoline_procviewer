2d686ed3d21700e22a580061dda8d485b258d14f
KosukeTakahashi <kilotango37@icloud.com>
2022-10-13 13:35:38 +0900

    first commit

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.write("Hello, World from TypeScript!")})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiJtQkFBQUEsU0FBU0MsTUFBTSxnQyIsInNvdXJjZXMiOlsid2VicGFjazovL2F1dG9saW5lX3Byb2N2aWV3ZXIvLi9zcmMvbWFpbi50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC53cml0ZSgnSGVsbG8sIFdvcmxkIGZyb20gVHlwZVNjcmlwdCEnKSJdLCJuYW1lcyI6WyJkb2N1bWVudCIsIndyaXRlIl0sInNvdXJjZVJvb3QiOiIifQ==
